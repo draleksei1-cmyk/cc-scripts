d73e82551965f4c91afe445df06a28beca9f13a1
draleksei1-cmyk <dr.aleksei1@gmail.com>
2026-05-24 21:02:54 +0300

    Create manifest.lua

---
+++ manifest.lua
@@ -0,0 +1,25 @@
+return {
+  update = {
+    path = "update.lua",
+    target = "update",
+    desc = "Updater itself"
+  },
+
+  tunnel = {
+    path = "programs/tunnel.lua",
+    target = "tunnel",
+    desc = "Tunnel mining turtle"
+  },
+
+  quarry = {
+    path = "programs/quarry.lua",
+    target = "quarry",
+    desc = "Quarry mining turtle"
+  },
+
+  shaft = {
+    path = "programs/shaft.lua",
+    target = "shaft",
+    desc = "Vertical shaft turtle"
+  }
+}
